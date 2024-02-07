06647639225a218daf52bcd0356aea825459bb2e
Anna <anasoft@yandex.ru>
2024-02-08 00:11:51 +0500

    Homework 8

--- logger.py
+++ logger.py
@@ -1,4 +1,5 @@
 from data_create import input_user_data
+import itertools
 
 def input_data():
     name, surname, phone, adress = input_user_data()
@@ -32,3 +33,18 @@ def print_data():
     with open('data_second_variant.csv', 'r', encoding='utf-8') as file:
         data = file.readlines()
         print(''.join(data))
+
+def copy_data():
+    
+    n_zap = int(input('Введите номер записи: '))
+
+    with open('data_first_variant.csv', 'r', encoding='utf-8') as file1:
+        lines = file1.readlines()[(n_zap-1) * 5:(n_zap) * 5 - 1]
+        
+        name = lines[0][:-1]
+        surname = lines[1][:-1]
+        phone = lines[2][:-1]
+        adress = lines[3][:-1]
+            
+        with open('data_second_variant.csv', 'a', encoding='utf-8') as file2:
+            file2.write(f'{name};{surname};{phone};{adress}\n\n')
--- ui.py
+++ ui.py
@@ -1,18 +1,21 @@
-from logger import input_data, print_data
+from logger import input_data, print_data, copy_data
 
 def interface():
     print('Добрый день! Я бот-помощник. \n'
           'Что Вы хотите сделать? \n'
           '1 - Записать данные \n'
-          '2 - Вывести данные')
+          '2 - Вывести данные \n'
+          '3 - Скопировать данные \n')
     command = int(input('Ваш выбор: '))
 
-    while command < 1 or command > 2:
+    while command < 1 or command > 3:
         command = int(input('Ошибка! Ваш выбор: '))
 
     if command == 1:
         input_data()
     elif command == 2:
         print_data()
+    elif command == 3:
+        copy_data()
 
 interface()
